8db6b0274e42c9bdec0a661b6e4b0b2df4606ef0
jdhova <juud0010@gmail.com>
2023-07-16 13:39:08 -0300

    where-clu

--- basics.sql
+++ basics.sql
@@ -2,7 +2,7 @@
 
 
 
--- QUESTION 1
+-- QUESTION 1 WHERE CLAUSE
 -- SELECT EMPLOYEES ,GENDER, MARITAL STATUS WITH JOB TITLE 'Marketing Assistant' AND 'Tool Designer' 
 
 SELECT JobTitle  as 'Employees' 
@@ -14,7 +14,58 @@ where JobTitle =  'Marketing Assistant'
 or JobTitle= 'Tool Designer' 
 
 
---QUESTION 2
+-- RETURN THE JOB TITLE AND ORGANIZATIONAL LEVELS GROUP BY JOB TITLE AND LEVEL
+-- AND RETURN THE ORG LEVELS GREATED THAN 3 AND ORDER IN DECENDING
+-- GROUP BY ORDER BY AND HAVING CLAUSE
+
+SELECT JobTitle as 'Title'
+		, OrganizationLevel as 'Level'
+		
+FROM [HumanResources].[Employee]
+group by JobTitle
+		,OrganizationLevel
+HAVING OrganizationLevel > 3
+order by OrganizationLevel desc
+
+
+-- JOIN TWO TABLES WITN INNER JOIN
+
+SELECT *		
+FROM [HumanResources].[Employee] emp
+inner JOIN [HumanResources].[EmployeePayHistory] pay
+on emp.BusinessEntityID = pay.BusinessEntityID
+
+
+--- WHERE CLAUSE-------------------------------------------------
+
+-- to be fixed
+SELECT VacationHours, SickLeaveHours, JobTitle	
+FROM [HumanResources].[Employee] emp
+inner JOIN [HumanResources].[EmployeePayHistory] pay
+on emp.BusinessEntityID = pay.BusinessEntityID
+WHERE VacationHours >= 70 and VacationHours <= 80
+
+-- between clause 
+
+SELECT VacationHours, SickLeaveHours, JobTitle	
+FROM [HumanResources].[Employee] emp
+inner JOIN [HumanResources].[EmployeePayHistory] pay
+on emp.BusinessEntityID = pay.BusinessEntityID
+WHERE VacationHours between 70 and 80 
+and SickLeaveHours between 60 and 80
+
+--LIKE CLAUSE 
+
+SELECT VacationHours, SickLeaveHours, JobTitle	
+FROM [HumanResources].[Employee] emp
+inner JOIN [HumanResources].[EmployeePayHistory] pay
+on emp.BusinessEntityID = pay.BusinessEntityID
+where emp.JobTitle LIKE 'Seni%' or  emp.JobTitle LIKE 'De%'
+
+
+
+
+--CTE---------------------------------------------------------------------
 -- FROM THE  [HumanResources].[Employee]
 --SELECT ALL EMOLOYEES WITH JOB TITLE MARKETING AND 'Tool Designer' AND HIRE DATE AFTER 2009
 
@@ -60,4 +111,4 @@ WITH NEWCTE AS
 	WHERE newyear > 2008 AND newyear < 2011
 		order by newyear, Gender DESC
 
-
+----------------------------------------------------------------------
